e0874a8c45d2b3da63faa3e4c6d5330bb1d4a7fe
梁伟 <nmgliangwei@gmail.com>
2023-11-15 22:07:25 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,23 +5,9 @@ set -e
 mkdir -p build/stand
 mkdir -p build/raw
 
-VERSION=`curl https://raw.githubusercontent.com/metowolf/qqwry.dat/main/version.json | jq -r .latest`
-wget https://github.com/metowolf/qqwry.dat/releases/download/$VERSION/qqwry.dat -4 -O build/qqwry.dat
+wget https://raw.githubusercontent.com/nmgliangwei/qqwry/main/version -O ./version
+VERSION=`cat version`
+wget https://github.com/nmgliangwei/qqwry/releases/download/$VERSION/qqwry.dat -4 -O build/qqwry.dat
 # node src/decode.js
 node src/packer.js
 node src/packer_raw.js
-
-LATEST_VERSION=`node src/version.js`
-CURRENT_VERSION=`curl https://registry.npmjs.org/qqwry.ipdb | jq -r '."dist-tags".latest'`
-
-if [ $CURRENT_VERSION != $LATEST_VERSION ]; then
-  cp README.md build/raw/README.md
-  cd build/raw
-  npm publish
-  cd ../..
-
-  cp README.md build/stand/README.md
-  cd build/stand
-  npm publish
-  cd ../..
-fi
